7768c8e04ad9064979f93004becdd3cf6c454115
Gabriel Dantas <gdantaslopes62@gmail.com>
2025-02-15 08:47:50 -0300

    feat: application backend

--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Survey(models.Model): 
+    number = models.IntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def str(self):
+        return self.description
--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from account.models import Survey
+from account.serializers import SurveySerializer
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def surveys(request):
+    if request.method == 'GET':
+        surveys = Survey.objects.all()
+        serializer = SurveySerializer(surveys, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SurveySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST) 
